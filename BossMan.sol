17d9d4be4e7bb9b49f679d487c75b3d5fa9614d5
Yates Randall <yates@myself.com>
2016-06-06 02:33:09 -0500

    Create BossMan.sol

---
+++ BossMan.sol
@@ -0,0 +1,29 @@
+contract owned {
+    function owned() {
+        owner = msg.sender;
+    }
+    modifier onlyowner() {
+        if (msg.sender != owner) throw;
+            _
+    }
+    address public owner;
+}
+
+contract PayCheck is owned {
+
+    uint public expiration = now + 1 weeks;
+    uint public value = 10 ** 18;
+
+    function changeOwner(address _newOwner) onlyowner  {
+        owner = _newOwner;
+    }
+
+    function Redeem() onlyowner {
+        if (block.timestamp > expiration) {
+
+        msg.sender.send(value);
+        expiration = now + 1 weeks;
+
+        }
+    }
+}
