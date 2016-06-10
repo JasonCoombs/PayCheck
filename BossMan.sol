7340da869a0dc9302c7091fbaa50de7664890a83
Yates Randall <yates@myself.com>
2016-06-09 19:43:31 -0500

    Update BossMan.sol

--- BossMan.sol
+++ BossMan.sol
@@ -18,7 +18,8 @@ contract owned {
 contract PayCheck is owned {
 
     uint public n_days = 1;
-    uint public expiration = now + (n_days * days);
+    uint public sdays = 86400;
+    uint public expiration = now + (n_days * sdays);
     uint public value = 10 ** 18;
 
     function changeOwner(address _newOwner) onlyowner  {
@@ -26,7 +27,11 @@ contract PayCheck is owned {
     }
     function changeN_days(uint _newN_days) onlyowner  {
         n_days = _newN_days;
-        expiration = now + (n_days * days);
+        expiration =  now + (n_days * sdays);
+    }
+    function changeSdays(uint _newSdays) onlyowner  {
+        sdays = _newSdays;
+        expiration =  now + (n_days * sdays);
     }
     function changePay(uint _newPay) onlyowner  {
         value = _newPay;
@@ -34,12 +39,14 @@ contract PayCheck is owned {
     function changeStaff(address _newStaff) onlyowner  {
         staff = _newStaff;
     }
-
     function Redeem() onlystaff {
         if (block.timestamp > expiration) {
         staff.send(value);  
-        expiration = now + (n_days * days);
-
+        expiration = now + (n_days * sdays);
         }
+    }
+    function Kill() onlyowner { 
+        if (msg.sender != owner) throw;
+        suicide(owner); 
     }
 }
